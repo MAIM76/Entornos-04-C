f002a2c722434c560c2a4992df1b08d1f6138e90
Miguel <Miguel@gmail.com>
2025-02-18 17:19:59 +0100

    Saludo 3 añadido

--- src/miguelangelgui/git/MiguelAngelGUIGIT.java
+++ src/miguelangelgui/git/MiguelAngelGUIGIT.java
@@ -16,10 +16,13 @@ extends JFrame {
 public MiguelAngelGUIGIT() {
 JLabel lblSaludo = new JLabel("Hola Mundo.");
 JLabel lblSaludo2 = new JLabel("¿Qué tal estás?.");
+JLabel lblSaludo3 = new JLabel("¿Hola?.");
 add(lblSaludo);
 lblSaludo.setBounds(2,28,116,24);
 add(lblSaludo2);
 lblSaludo2.setBounds(2,50,116,24);
+add(lblSaludo3);
+lblSaludo3.setBounds(2,75,116,24);
 this.setSize(400, 300);
 this.setTitle("JFrame");
 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
